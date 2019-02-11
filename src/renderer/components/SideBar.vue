c6fb33552153a6ccac105c1861e46cdc5e83e4d9
Pellé Ewann <ewann.pelle@epita.fr>
2019-02-11 22:50:20 +0100

    updated sidebar methods

--- src/renderer/components/SideBar.vue
+++ src/renderer/components/SideBar.vue
@@ -1,24 +1,15 @@
 <template>
-  <v-navigation-drawer
-    dark
-    permanent
-    mini-variant
-    fixed
-    class="sidebar"
-  >
+  <v-navigation-drawer dark permanent mini-variant fixed class="sidebar">
     <v-list>
-      <v-list-tile
-        v-for="item in items"
-        :key="item.title"
-        @click=""
-      >
+      <v-list-tile @click="getPath()" title="Reparer">
         <v-list-tile-action>
-          <v-icon>{{ item.icon }}</v-icon>
+          <v-icon>build</v-icon>
         </v-list-tile-action>
-
-        <v-list-tile-content>
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile @click="getPath()" title="Changer de dossier d'installation">
+        <v-list-tile-action>
+            <v-icon>folder</v-icon>
+        </v-list-tile-action>
       </v-list-tile>
     </v-list>
   </v-navigation-drawer>
@@ -26,21 +17,36 @@
 
 <script>
   export default {
+    inherit: true,
     data () {
       return {
         drawer: true,
-        items: [
-          { title: 'Home', icon: 'dashboard' },
-          { title: 'Reparer', icon: 'build' }
-        ],
         right: null
+      }
+    },
+    methods: {
+      getPath () {
+        const {
+          dialog
+        } = require('electron').remote
+        let path = dialog.showOpenDialog({
+          properties: ['openDirectory']
+        })
+        if (path[0]) {
+          let data = JSON.stringify(path[0])
+          let fs = require('fs')
+          fs.writeFileSync('./static/json/path.json', data)
+          this.path = path
+        }
+      },
+      getEmpty () {
       }
     }
   }
 </script>
 
 <style>
-  .sidebar{
+  .sidebar {
     margin-top: 53px!important;
   }
 </style>
